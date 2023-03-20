fd88a159d86c713b7e2800f10fc9a4e0050ebbd7
TKalo <troelskaldau@live.dk>
2023-03-20 19:16:49 +0100

    fixed auth session endpoints

--- api/src/auth-session/auth-session.controller.ts
+++ api/src/auth-session/auth-session.controller.ts
@@ -1,29 +1,37 @@
 import { Metadata } from '@grpc/grpc-js/build/src/metadata';
 import { Controller, UseGuards } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
-import { AuthTokens, AuthSessionServiceController, AuthSessionServiceControllerMethods, AuthSessionServiceError, Void } from 'generated_proto/hero';
+import {
+  AuthTokens,
+  AuthSessionServiceController,
+  AuthSessionServiceControllerMethods,
+  AuthSessionServiceError,
+  Void,
+} from 'generated_proto/hero';
 
 import { Observable } from 'rxjs';
 import { AuthGuard } from 'src/common/auth.guard';
 import { AuthSessionService } from './auth-session.service';
 import { AuthSessionGuard } from './guards/auth-session.guard';
 
-
 @Controller()
 @AuthSessionServiceControllerMethods()
 export class AuthSessionController implements AuthSessionServiceController {
   constructor(private readonly authSessionService: AuthSessionService) {}
 
-  @UseGuards(AuthGuard)  
-  signOut(request: Void, metadata: Metadata): Void | Observable<Void> | Promise<Void> {
+  @UseGuards(AuthGuard)
+  signOut(
+    request: Void,
+    metadata: Metadata,
+  ): Void | Observable<Void> | Promise<Void> {
     return new Promise(async () => {
-      const meta = metadata.getMap();
-      const authHeader = meta['authentication'];
-      const token = authHeader ? authHeader[0] : undefined;
-
+      const authToken = metadata
+        .get('authentication')
+        ?.toString()
+        ?.replace(/^Bearer\s/, '');
 
       try {
-        await this.authSessionService.signOut(token[0]);
+        await this.authSessionService.signOut(authToken);
         return {};
       } catch (e) {
         throw new RpcException(
@@ -36,12 +44,16 @@ export class AuthSessionController implements AuthSessionServiceController {
   @UseGuards(AuthSessionGuard)
   refreshAccessToken(
     request: Void,
+    metadata: Metadata,
   ): AuthTokens | Observable<AuthTokens> | Promise<AuthTokens> {
     return new Promise(async () => {
+      const authToken = metadata
+        .get('authentication')
+        ?.toString()
+        ?.replace(/^Bearer\s/, '');
+
       try {
-        const token = await this.authSessionService.refreshAccessToken(
-          "",
-        );
+        const token = await this.authSessionService.refreshAccessToken(authToken);
         return {
           token: token,
         };
