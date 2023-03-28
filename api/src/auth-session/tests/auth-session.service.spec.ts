3c8a307ada13197d1de8a2900652a12ab5e795c1
TKalo <troelskaldau@live.dk>
2023-03-28 15:26:46 +0200

    modified auth session test environment

--- api/src/auth-session/tests/auth-session.service.spec.ts
+++ api/src/auth-session/tests/auth-session.service.spec.ts
@@ -1,42 +1,48 @@
 import { JwtService } from '@nestjs/jwt';
-import { ObjectId } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
+import { MongoService } from '../../common/mongo.service';
+import { init, TestContext } from '../../common/test-environment';
 
 import { AuthSessionService } from '../auth-session.service';
-import { init, TestContext } from './auth-session.test-environment';
+import { InvalidRefreshTokenError } from '../errors/invalid-refresh-token.error';
+import { AuthSession } from '../types/auth-session.entity';
 
 describe('AuthSessionService', () => {
   let service: AuthSessionService;
-  let env: TestContext;
+  let context: TestContext;
   let jwtService: JwtService;
+  let collection: Collection<AuthSession>;
 
   beforeAll(async () => {
-    env = await init();
-    service = env.t.get<AuthSessionService>(AuthSessionService);
-    jwtService = env.t.get<JwtService>(JwtService);
+    context = await init();
+    service = context.t.get<AuthSessionService>(AuthSessionService);
+    jwtService = context.t.get<JwtService>(JwtService);
+    const mongoService = context.t.get<MongoService>(MongoService);
+    collection = mongoService.getCollection(AuthSession.name)
   });
 
   afterAll(async () => {
-    await env.mongoClient.close();
-    await env.mongoServer.stop();
+    await context.mongoClient.close();
+    await context.mongoServer.stop();
   });
 
   afterEach(async () => {
-    await env.collection.deleteMany({});
+    await collection.deleteMany({});
   });
 
-  it('create session - when userid not associated with existing auth session is given, expect session to be created', async () => {
+  it('createSession - when userid not associated with existing auth session is given, expect session to be created', async () => {
     const userId = new ObjectId();
-    const token = await service.createSession(userId);
+    const tokens = await service.createSession(userId);
 
-    expect(token != null).toBeTruthy();
+    expect(tokens != null).toBeTruthy();
 
-    const session = await env.collection.findOne({
+    const session = await collection.findOne({
       userId: userId,
     });
 
     expect(session != null).toBeTruthy();
 
-    expect(session.refreshToken).toBe(token);
+    expect(session.refreshToken).toBe(tokens.refreshToken);
 
     const tenDaysFromNow = new Date();
     tenDaysFromNow.setDate(tenDaysFromNow.getDate() + 10);
@@ -44,53 +50,104 @@ describe('AuthSessionService', () => {
     expect(session.expiresAt.getDate()).toBe(tenDaysFromNow.getDate());
   });
 
-  it('sign out', async () => {
+  it('signOut - when token exists, should remove session', async () => {
     const userId = new ObjectId();
-    const token = await service.createSession(userId);
+    const tokens = await service.createSession(userId);
 
-    expect(token != null).toBeTruthy();
+    expect(tokens != null).toBeTruthy();
 
-    let session = await env.collection.findOne({
+    let session = await collection.findOne({
       userId: userId,
     });
 
     expect(session != null).toBeTruthy();
 
-    await service.signOut(token);
+    await service.signOut(tokens.refreshToken);
 
-    session = await env.collection.findOne({
+    session = await collection.findOne({
       userId: userId,
     });
 
     expect(session == null).toBeTruthy();
   });
 
-  it('refresh access token - with valid token', async () => {
+  it('signOut - when token does not exist, should act as if it was removed', async () => {
     const userId = new ObjectId();
-    const token = await service.createSession(userId);
+    const token = "non existing token";
 
-    expect(token != null).toBeTruthy();
 
-    const session = await env.collection.findOne({
+    await service.signOut(token);
+
+    const session = await collection.findOne({
+      refreshToken: token,
+    });
+
+    expect(session == null).toBeTruthy();
+  });
+
+
+  it('refreshAccessToken - when token is valid, should return accessToken', async () => {
+    const userId = new ObjectId();
+    const tokens = await service.createSession(userId);
+
+    expect(tokens != null).toBeTruthy();
+
+    const session = await collection.findOne({
       userId: userId,
     });
 
     expect(session != null).toBeTruthy();
 
-    const jwt = await service.refreshAccessToken(token);
+    const jwt = await service.refreshAccessToken(tokens.refreshToken);
 
     const decodedJwt = jwtService.verify(jwt);
 
     expect(decodedJwt.userId).toBe(userId.toHexString())
   });
 
-  it('refreshTokenGuard - with invalid token', async () => {
+  it('refreshTokenGuard - When token is invalid, should throw error', async () => {
     const token = "invalid token"
 
     const refreshTokenGuard = async () => {
       await service.refreshTokenGuard(token);
     };
 
-    expect(refreshTokenGuard()).rejects.toThrow('invalid refresh token')
+    expect(refreshTokenGuard()).rejects.toThrowError(InvalidRefreshTokenError);
+  });
+
+  it('refreshTokenGuard - When token is expired, should throw error', async () => {
+    const token = "valid"
+    const expiration = new Date();
+
+    collection.insertOne({
+      userId: new ObjectId(),
+      refreshToken: token,
+      expiresAt: expiration
+    })
+
+    const refreshTokenGuard = async () => {
+      await service.refreshTokenGuard(token);
+    };
+
+    expect(refreshTokenGuard()).rejects.toThrowError(InvalidRefreshTokenError);
+  });
+
+  it('refreshTokenGuard - When token is valid, should complete without error', async () => {
+    const userId = new ObjectId();
+    const tokens = await service.createSession(userId);
+
+    expect(tokens != null).toBeTruthy();
+
+    const session = await collection.findOne({
+      userId: userId,
+    });
+
+    expect(session != null).toBeTruthy();
+
+    const refreshTokenGuard = async () => {
+      await service.refreshTokenGuard(tokens.refreshToken);
+    };
+
+    expect(refreshTokenGuard()).resolves.not.toThrowError()
   });
 });
