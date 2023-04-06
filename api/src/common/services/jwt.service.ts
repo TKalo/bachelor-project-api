2c19172db4d0fd086e2794b2ab162b98826d3b08
TKalo <troelskaldau@live.dk>
2023-04-06 13:36:08 +0200

    created jwt service in common

---
+++ api/src/common/services/jwt.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { ObjectId } from 'mongodb';
+
+@Injectable()
+export class JwtHandlerService {
+  constructor(
+    private readonly jwtService: JwtService,
+  ) {}
+
+
+  generateAccessToken(userId: ObjectId): string {
+    return this.jwtService.sign({
+      userId: userId.toHexString(),
+    });
+  }
+
+  //check access token is valid and within expiration
+  validateAccessToken(token: string): boolean {
+    try {
+        this.jwtService.verify(token);
+        return true;
+      } catch (error) {
+        return false;
+      }
+  }
+  //get userId from access token
+  decodeAccessToken(token: string): ObjectId {
+    const decodedToken = this.jwtService.decode(token);
+    const userIdHex = decodedToken['userId'];
+    return new ObjectId(userIdHex);
+  }
+}
