283e0fd7e73bc6eec5419961e03ecdd69ab81cd9
TKalo <troelskaldau@live.dk>
2023-04-06 13:36:52 +0200

    created integration testing scripts

---
+++ integration-test-script.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+docker-compose -f docker-compose-integration.yaml down -v
+
+docker-compose -f docker-compose-integration.yaml build
+
+docker-compose -f docker-compose-integration.yaml up mongo -d
+timeout /t 4 >nul
+docker-compose -f docker-compose-integration.yaml up api -d
+timeout /t 10 >nul
+docker-compose -f docker-compose-integration.yaml up client
---
+++ integration-test-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker-compose -f docker-compose-integration.yaml down -v
+
+docker-compose -f docker-compose-integration.yaml build
+
+docker-compose -f docker-compose-integration.yaml up mongo -d
+sleep 4
+
+docker-compose -f docker-compose-integration.yaml up api -d
+sleep 10
+
+docker-compose -f docker-compose-integration.yaml up client
