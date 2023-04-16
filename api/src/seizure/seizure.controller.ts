4469070b4e5bf93d28cde502825ffab8d5c91166
TKalo <troelskaldau@live.dk>
2023-04-16 12:32:35 +0200

    implemented seizure controller

--- api/src/seizure/seizure.controller.ts
+++ api/src/seizure/seizure.controller.ts
@@ -1,18 +1,23 @@
 import { Controller, UseGuards } from '@nestjs/common';
 import {
-    Profile,
-    ProfileChange,
+    Seizure,
+    SeizureChange,
+    SeizureFilter,
+    SeizureList,
     SeizureServiceController,
     SeizureServiceControllerMethods,
-    Void
+    Void,
 } from '../../generated_proto/hero';
 
 import { Metadata } from '@grpc/grpc-js';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { ValidatedGuard } from 'src/common/guards/validated.guard';
 import { GrpcService } from 'src/common/services/grpc.service';
+import { SeizureType } from '../../generated_proto/hero';
+import { SeizureDataInsufficientError } from './errors/seizure-data-insufficient.error';
+import { SeizureDataInsufficientGrpcError } from './errors/seizure-data-insufficient.grpc-error';
+import { SeizureInternalGrpcError } from './errors/seizure-internal.grpc-error';
 import { SeizureService } from './seizure.service';
-
 
 @UseGuards(ValidatedGuard)
 @Controller()
@@ -22,17 +27,113 @@ export class SeizureController implements SeizureServiceController {
     private readonly service: SeizureService,
     private readonly grpcService: GrpcService,
   ) {}
-    create(request: Profile, metadata?: Metadata): Void | Promise<Void> | Observable<Void> {
-        throw new Error('Method not implemented.');
-    }
-    delete(request: Profile, metadata?: Metadata): Void | Promise<Void> | Observable<Void> {
-        throw new Error('Method not implemented.');
-    }
-    get(request: Void, metadata?: Metadata): Profile | Promise<Profile> | Observable<Profile> {
-        throw new Error('Method not implemented.');
-    }
-    stream(request: Void, metadata?: Metadata): Observable<ProfileChange> {
-        throw new Error('Method not implemented.');
-    }
+  create(
+    request: Seizure,
+    metadata?: Metadata,
+  ): Void | Promise<Void> | Observable<Void> {
+    return new Promise<Void>(async (resolve, reject) => {
+      try {
+        this.service.validation(request.durationSeconds, request.type);
 
+        const accessToken = this.grpcService.extractToken(metadata);
+
+        await this.service.create(
+          accessToken,
+          request.type,
+          request.durationSeconds,
+        );
+
+        resolve({});
+      } catch (e) {
+        if (e.message === new SeizureDataInsufficientError().message) {
+          reject(new SeizureDataInsufficientGrpcError());
+        } else {
+          reject(new SeizureInternalGrpcError());
+        }
+      }
+    });
+  }
+  delete(
+    request: Seizure,
+    metadata?: Metadata,
+  ): Void | Promise<Void> | Observable<Void> {
+    return new Promise<Void>(async (resolve, reject) => {
+      try {
+        this.service.validation(request.durationSeconds, request.type);
+
+        const accessToken = this.grpcService.extractToken(metadata);
+
+        await this.service.delete(accessToken, request.id);
+
+        resolve({});
+      } catch (e) {
+        if (e.message === new SeizureDataInsufficientError().message) {
+          reject(new SeizureDataInsufficientGrpcError());
+        } else {
+          reject(new SeizureInternalGrpcError());
+        }
+      }
+    });
+  }
+  get(
+    request: SeizureFilter,
+    metadata?: Metadata,
+  ): SeizureList | Promise<SeizureList> | Observable<SeizureList> {
+    return new Promise<SeizureList>(async (resolve, reject) => {
+      try {
+        const accessToken = this.grpcService.extractToken(metadata);
+
+        const seizures = await this.service.get(
+          accessToken,
+          request.durationSecondsFrom,
+          request.durationSecondsTo,
+        );
+
+        const grpcSeizures: Seizure[] = seizures.map((value) => {
+          return {
+            id: value._id.toHexString(),
+            type: SeizureType[SeizureType[value.type]],
+            durationSeconds: value.duration,
+          };
+        });
+
+        resolve({
+          seizures: grpcSeizures,
+        });
+      } catch (e) {
+        if (e.message === new SeizureDataInsufficientError().message) {
+          reject(new SeizureDataInsufficientGrpcError());
+        } else {
+          reject(new SeizureInternalGrpcError());
+        }
+      }
+    });
+  }
+  stream(
+    request: SeizureFilter,
+    metadata?: Metadata,
+  ): Observable<SeizureChange> {
+    try {
+      const accessToken = this.grpcService.extractToken(metadata);
+
+      const stream = this.service.stream(
+        accessToken,
+        request.durationSecondsFrom,
+        request.durationSecondsTo,
+      );
+
+      return stream.pipe<SeizureChange>(
+        map((data) => ({
+          change: data.change,
+          seizure: {
+            id: data.seizure._id.toHexString(),
+            type: SeizureType[SeizureType[data.seizure.type]],
+            durationSeconds: data.seizure.duration,
+          }
+        })),
+      );
+    } catch (e) {
+      throw new SeizureInternalGrpcError();
+    }
+  }
 }
